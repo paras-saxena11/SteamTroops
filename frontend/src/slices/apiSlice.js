5cdb1798825411f97239ddd6281a9fbe1308e904
Paras Saxena <paras.saxena@qodeit.space>
2023-08-16 20:03:07 +0530

    ADD

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,8 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:5000/api",
+  baseUrl: "https://steamtroops.onrender.com",
+  // baseUrl: "http://localhost:5000/api",
 });
 
 export const apiSlice = createApi({
