9636864366e035c712c459707141da82a1cc2c77
paras-saxena11 <77022754+paras-saxena11@users.noreply.github.com>
2023-08-16 20:07:59 +0530

    Update apiSlice.js

--- frontend/src/slices/apiSlice.js
+++ frontend/src/slices/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://steamtroops.onrender.com",
+  baseUrl: "https://steamtroops.onrender.com/api",
   // baseUrl: "http://localhost:5000/api",
 });
 
